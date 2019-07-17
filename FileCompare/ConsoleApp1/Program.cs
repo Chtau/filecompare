ea01b6ba51abf266ccd1cadce364963ef76aa171
Christoph Taucher <taucher.ch@gmail.com>
2019-07-17 10:35:22 +0200

    basic log output

--- FileCompare/ConsoleApp1/Program.cs
+++ FileCompare/ConsoleApp1/Program.cs
@@ -7,22 +7,42 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string path = @"D:\Downloads";
-            var dup = new Duplicates();
-            dup.Collect(path).GetAwaiter().GetResult();
-            Console.WriteLine($"Files found: " + dup.Files.Count);
-            dup.PrepareCompareValues += (object sender, bool e) =>
+            string appStartPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            if (args.Length > 0)
             {
-                if (e)
-                    Console.WriteLine("Prepare compare value complete");
-                else
-                    Console.WriteLine("Prepare compare value starting");
-            };
-            dup.ProcessFile += (object sender, string e) =>
+                string path = args[0];
+                var dup = new Duplicates();
+                dup.Collect(path).GetAwaiter().GetResult();
+                Console.WriteLine($"Files found: " + dup.Files.Count);
+                dup.PrepareCompareValues += (object sender, bool e) =>
+                {
+                    if (e)
+                        Console.WriteLine("Prepare compare value complete");
+                    else
+                        Console.WriteLine("Prepare compare value starting");
+                };
+                dup.ProcessFile += (object sender, string e) =>
+                {
+                    Console.WriteLine("Process file: " + e);
+                };
+                var result = dup.Find().GetAwaiter().GetResult();
+                if (result != null && result.Count > 0)
+                {
+                    string outputPath = System.IO.Path.Combine(appStartPath, "compare_result.log");
+                    foreach (var item in result)
+                    {
+                        System.IO.File.AppendAllText(outputPath, "Similar File:" + item.FileResults.Count.ToString() + Environment.NewLine);
+                        foreach (var file in item.FileResults)
+                        {
+                            System.IO.File.AppendAllText(outputPath, file.ToString() + Environment.NewLine);
+                        }
+                        System.IO.File.AppendAllText(outputPath, Environment.NewLine + Environment.NewLine);
+                    }
+                }
+            } else
             {
-                Console.WriteLine("Process file: " + e);
-            };
-            var result = dup.Find().GetAwaiter().GetResult();
+                Console.WriteLine("No Path to check provided");
+            }
             Console.ReadKey();
         }
 
