68ede9838bd642fec23c02cef1529dae86a2a7e5
Christoph Taucher <taucher.ch@gmail.com>
2019-08-21 20:46:30 +0200

    add interface to call the implementation for the compare library

---
+++ FileCompare/Client/Features/JobService/CompareInAppHosting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.Features.Jobs.Models;
+
+namespace Client.Features.JobService
+{
+    public class CompareInAppHosting : ICompare
+    {
+        public event EventHandler<CompareProgressEventArgs> ReportProgress;
+
+        public void StartJob(Job job, JobConfiguration config)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopJob(Job job, JobConfiguration config)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FileCompare/Client/Features/JobService/ICompare.cs
@@ -0,0 +1,32 @@
+﻿using Client.Features.Jobs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Features.JobService
+{
+    public interface ICompare
+    {
+        event EventHandler<CompareProgressEventArgs> ReportProgress;
+        void StartJob(Job job, JobConfiguration config);
+        void StopJob(Job job, JobConfiguration config);
+    }
+
+    public class CompareProgressEventArgs : EventArgs
+    {
+        public Job Job { get; set; }
+        public JobConfiguration JobConfiguration { get; set; }
+        public List<object> ProgressData { get; set; }
+        public List<string> ProgressInfoText { get; set; }
+
+        public CompareProgressEventArgs(Job job, JobConfiguration jobConfiguration, List<object> progressData = null, List<string> progressInfoText = null)
+        {
+            Job = job;
+            JobConfiguration = jobConfiguration;
+            ProgressData = progressData ?? new List<object>();
+            ProgressInfoText = progressInfoText ?? new List<string>();
+        }
+    }
+}
