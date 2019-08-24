ee45f51d7764027797fa750f787ad28403c1794a
Christoph Taucher <taucher.ch@gmail.com>
2019-08-24 14:56:37 +0200

    add duplicate result

---
+++ FileCompare/Client/Features/Duplicates/ViewModels/DuplicatesResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Features.Duplicates.ViewModels
+{
+    public class DuplicatesResult
+    {
+        public Guid DuplicateId { get; set; }
+        public int DuplicateValue { get; set; }
+        public List<Folders.Models.CollectPath> Paths { get; set; }
+    }
+}
