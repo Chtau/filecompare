278e15bb8c6034d7dd285afe145c6861467067d3
Christoph Taucher <taucher.ch@gmail.com>
2019-08-31 11:40:05 +0200

    add converter to show enum flag text value for compare result

---
+++ FileCompare/Client/Internal/CompareResultValueConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Client.Internal
+{
+    public class CompareResultValueConverter : IValueConverter
+    {
+        public CompareResultValueConverter()
+        {
+            
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                if (int.TryParse(value.ToString(), out int eValue))
+                {
+                    return (Compare.CompareValue.Types)eValue;
+                }
+            }
+            return value?.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
